59f902c1ba2a7920eb1850e2694117c22990c053
SaineeNihar <nihar.sainee@gmail.com>
2024-06-23 13:43:29 +0530

    df

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
-const heading = React.createElement(
-  "h1",
-  { id: "head", cyz: "dgdg" },
-  "Hello From react"
-);
+const heading = React.createElement("h1", { id: "head", cyz: "dgdg" }, [
+  React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "Hello From react")
+  ),
+  React.createElement("h2", {}, "Hello From react"),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 console.log(heading);
