6921680f0568254d1ca09708ff3a69715c13af34
SaineeNihar <nihar.sainee@gmail.com>
2024-06-23 13:28:40 +0530

    first push with basics of react

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement(
+  "h1",
+  { id: "head", cyz: "dgdg" },
+  "Hello From react"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+console.log(heading);
